befef7aedca5f202a1435613745076a6098fde3a
Björn Mattsson <bjorn@sunet.se>
2023-06-30 13:47:58 +0200

    Changed spacing

--- sql/database.sql
+++ sql/database.sql
@@ -1,13 +1,15 @@
-CREATE TABLE `params` (
-	`instance` varchar(20) DEFAULT NULL,
-	`id` varchar(20) DEFAULT NULL,
-	`value` text DEFAULT NULL
-);
-
 CREATE TABLE `invites` (
-	`instance` varchar(20) DEFAULT NULL,
-	`hash` varchar(20) DEFAULT NULL,
-	`session` varchar(27) DEFAULT NULL,
-	`modified` datetime DEFAULT NULL,
-	`attributes` text DEFAULT NULL
-);
+  `instance` varchar(20) DEFAULT NULL,
+  `hash` varchar(20) DEFAULT NULL,
+  `session` varchar(27) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `attributes` text DEFAULT NULL
+)
+
+CREATE TABLE `params` (
+  `instance` varchar(20) DEFAULT NULL,
+  `id` varchar(20) DEFAULT NULL,
+  `value` text DEFAULT NULL
+)
+
+INSERT INTO `params` (`instance`, `id`, `value`) VALUES ('', 'dbVersion', '1');
